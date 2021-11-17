88ec4386e0dc1944a61d17267296f3b3c9b1e447
hugo.heer <hugo.heer@epita.fr>
2021-11-17 20:35:18 +0100

    LevelC done

--- LevelC/RotationString.php
+++ LevelC/RotationString.php
@@ -15,7 +15,18 @@ class RotationString
     public static function isRotation($s1, $s2)
     {
         /** @TODO */
+        $s1copy = $s1;
+        for ($i = 0; $i < strlen($s1); $i++) {
+            if (strcmp($s1copy, $s2) == 0) {
+                return true;
+            }
+            $tmp = $s1copy[0];
 
+            for ($j = 0; $j < strlen($s1) - 1; $j++) {
+                $s1copy[$j] = $s1copy[$j + 1];
+            }
+            $s1copy[strlen($s1) - 1] = $tmp;
+        }
         return false;
     }
 
